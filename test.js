e3105e5e05752549aad6f5dd20e225904afa06ee
travisbright <travis.bright1994@gmail.com>
2019-02-12 12:12:09 -0500

    replaced and condition with the or condition, changed message

--- test.js
+++ test.js
@@ -1,14 +1,10 @@
-var myAge = 8;
+var myAge = 26;
 
-if(myAge>30){
-document.write("you are over 30!");
+if(myAge<18||myAge>30||myAge===25){
+    document.write("you aint coming!");
 
-} else if(myAge>20){
-    document.write("you are over 20!");
+} else {
+    document.write("you can come, you cool dude!");
+    
 
-} else if(myAge>10){
-document.write("you are over 10!");
-
-} else{
-    document.write("you are not over 10.");
-}
+} 
