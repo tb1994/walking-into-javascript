bc74eb791c65c579ac949a16da1ae3a92ad6cfe7
travisbright <travis.bright1994@gmail.com>
2019-02-25 11:16:49 -0500

    added an array to test.js swapped link 6 for link 0 in html

--- test.js
+++ test.js
@@ -1,21 +1,7 @@
+var links = document.getElementsByTagName("a");
 
+for(i = 0; i < links.length; i++){
 
-for (i = 0; i < 10; i++)
-
-{
-
-if(i===5 || i===3){
-    continue;
-
+links[i].className = "link-" + i;
 
 }
-
-    console.log(i);
-  
-    if(i===7){
-break; 
-
-    }
-}
-
-console.log("i have broken out of the loop.");
