eedeaf5094a8330be848f63f62c5d89c8f8af3e3
travisbright <travis.bright1994@gmail.com>
2019-02-25 23:48:23 -0500

    created some global functions in test.js file

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-var links = document.getElementsByTagName("a");
+var average = 0;
 
-for(i = 0; i < links.length; i++){
-
-links[i].className = "link-" + i;
+function getAverage (a,b,c,d,e){
+    average = (a+b+c+d+e)/5;
+    console.log(average);
+    return average;
 
 }
+
+var myResult = getAverage(7,12,25,40,121);
+
+
+function logResult(){
+    console.log("the average is " + myResult + " inside the function");
+
+}
+
+logResult();
